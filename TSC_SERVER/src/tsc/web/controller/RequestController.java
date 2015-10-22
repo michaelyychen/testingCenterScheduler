1cdb6aedddf1a5591e00fba15b2e0799ae17f8b0
Xiang Zhang <xzhangax@gmail.com>
2015-10-21 22:35:01 -0400

    SADSA

--- TSC_SERVER/src/tsc/web/controller/RequestController.java
+++ TSC_SERVER/src/tsc/web/controller/RequestController.java
@@ -4,15 +4,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import tsc.web.bean.ErrorBean;
+import tsc.web.bean.FeedBackBean;
 import tsc.web.bean.request.CreateReqRequestBean;
+import tsc.web.bean.request.CreateReqResponseBean;
+import tsc.web.bean.request.HttpResponseBean;
 import tsc.web.framework.Controller;
+import tsc.web.service.RequestService;
 
 public class RequestController implements Controller {
+
+	RequestService service;
+
+	public RequestController() {
+		// TODO Auto-generated constructor stub
+		service = new RequestService();
+	}
 
 	@Override
 	public void exec(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -20,26 +31,43 @@ public class RequestController implements Controller {
 		// TODO Auto-generated method stub
 		request.setAttribute("error", new ErrorBean(message));
 		request.getRequestDispatcher("/error.jsp").forward(request, response);
-		
+
 	}
 
 	@Override
 	public void webPageResponse() {
 		// TODO Auto-generated method stub
-		
-		
+
 	}
 
 	@Override
 	public void jsonDataResponse() {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	public void makeRequest(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		CreateReqRequestBean requestBean = new 
-		
+
 	}
 
+	public void makeRequest(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		CreateReqRequestBean requestBean = new CreateReqRequestBean(request);
+		if(requestBean.validData()){
+			CreateReqResponseBean responseBean = service.addRequest(requestBean);
+			
+			int result = responseBean.getResult();
+			
+			if(result== HttpResponseBean.SERVICE_SUCCESS){
+				
+				// Go to ....
+				
+			}else{
+				
+				FeedBackBean feedBack = responseBean.getFeedback();
+				errorPage(request, response,feedBack.getMessage());
+				// Go to error page
+			}
+		}
+			else{
+			
+			// go error page
+			}
+	}
 }
